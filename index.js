7ac52f99cc369ce82b1ab60b0a99e1c5907e842c
Alejandra Landa <98127121+aleLanda-rq@users.noreply.github.com>
2022-08-04 16:37:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,24 +3,68 @@ window.addEventListener('scroll', function()  {
 nav.classList.toggle('sticky', window.scrollY> 0);
 })
 
-
-
 document.querySelectorAll('.image_container img').forEach((image)=>{
-image.onclick = () =>{
-    document.querySelector('.pop_up_image').style.display ='block';
-    document.querySelector('.pop_up_image img').src=image.getAttribute('src')
-    document.querySelector('.sticky').style.display='none';
-}
-});
-
-document.querySelector('.pop_up_image span').onclick = ()=> {
+    image.addEventListener('click', ()=>{
+        document.querySelector('.pop_up_image').style.display ='block';
+        document.querySelector('.pop_up_image img').src=image.getAttribute('src')
+        document.querySelector('.sticky').style.display='none';
+    })
+})
+const popImage = document.querySelector('.pop_up_image span');
+if(popImage){
+popImage.addEventListener('click', () =>{
     document.querySelector('.pop_up_image').style.display ='none';
-    document.querySelector(".sticky").style.display='inline';
+        document.querySelector(".sticky").style.display='inline';
+})
 }
 
- document.querySelector('.load_button').addEventListener('click',() =>{
-console.log('I have been clicked')
- })
+
+const mobileNav=document.querySelector('.mobile-nav-links');
+const navBttn=document.querySelector('.btnimg');
+const closebttn=document.querySelector('.btnclose');
+const mobilenav = document.querySelector('.mobile-nav');
+
+
+if(navBttn){
+navBttn.addEventListener('click', ()=>{
+    navBttn.style.display='none';
+    mobileNav.style.display='block';
+    closebttn.style.display='block';
+    // mobileNav.style.background='rgba(0,0,0,0.5)';
+
+})
+}
+
+closebttn.addEventListener('click', ()=>{
+    mobileNav.style.display='none';
+    navBttn.style.display='block';
+    closebttn.style.display='none';
+})
+
+// navBttn.onclick(() =>{
+//     console.log('clicked')
+// mobileNav.style.display='block';
+// mobileNav.style.display='none';
+
+// })
+
+
+
+
+//  const navigationLinks = document.querySelector('.nav_Links');
+//  const hamburger = document.querySelector('.buttons');
+
+//  hamburger.addEventListener('click', () =>{
+//   hamburger.classList.toggle("active");
+//   navigationLinks.classList.toggle("active");
+//  })
+
+// document.querySelector('.').forEach(n => n.addEventListener('click', () => {
+//     hamburger.classList.toggle("active");
+//     navigationLinks.classList.toggle("active");
+// }))
+
+
 
 
 // const url="http://localhost:3000"
